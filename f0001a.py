13c974042b21519396d0182c0e7eb6211cf67b33
Tamas9c <55052371+Tamas9c@users.noreply.github.com>
2020-10-26 12:00:57 +0100

    Add files via upload

---
+++ f0001a.py
@@ -0,0 +1,12 @@
+'''
+Hibát fog kiírni mivel az idézőjel/aposztróf
+nincs bezárva.
+'''
+
+#Eredeti (hibás)
+print('...Hogyha én ezt tudtam volna)
+print('Köpönyeget hoztam vóna'!)
+
+#Javított (jó)
+print('...Hogyha én ezt tudtam volna')
+print('Köpönyeget hoztam vóna'!)
