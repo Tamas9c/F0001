64197c667a965da4d06cb18d695167d442017f5b
Tamas9c <55052371+Tamas9c@users.noreply.github.com>
2020-10-26 12:06:35 +0100

    Add files via upload

---
+++ f0001b.py
@@ -0,0 +1,11 @@
+'''
+F0001b: Mi történik, ha lefuttatjuk ezt a programot?
+
+Az elsőt simán kiírja, a másodikat pedig "" közé teszi
+mivel a két fajta aposztróf nem zárja ki egymást,
+ha 1-es aposztróffal kezded akkor azzal is kell befejezned
+köztük haszálhatod a 2-est mivel azt is szövegként érzékeli.
+'''
+
+print('Rásír a sok kis malac is!')
+print('"Én is, én is, én is, én is!"')
